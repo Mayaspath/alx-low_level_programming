9ff04cded818284fdd5bd5a43daa929ffa0a039b
Mayaspath <mayaspath@gmail.com>
2023-03-16 10:02:18 -0700

    my first line of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#!bin/bash
-/**
- * main int return
- *
- * printing a program with puts instead of printf
- */
 #include <stdio.h>
+/*
+ * main is my entry point annd the return is 0, my program prints a sentence using the function puts instead of printf
+ */
 int main(void)
 {
-	puts(" /"Programming is like building a multilingual puzzle");
-		return(0);
+	puts("/"Programming is like building a multilingual puzzle");
+		return (0);
 }
