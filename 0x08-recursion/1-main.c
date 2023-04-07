9b54287bda67696cc6a192eaf30a4da893cde1c4
Mayaspath <mayaspath@gmail.com>
2023-04-06 17:46:55 -0700

    Write a function that prints a string in reverse

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0 - Maya 07/04/2023
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
