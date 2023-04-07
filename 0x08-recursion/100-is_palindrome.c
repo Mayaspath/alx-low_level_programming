b24146aaa09c0419cdc7b21572884380720daf30
Mayaspath <mayaspath@gmail.com>
2023-04-06 18:21:10 -0700

    Write a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,12 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
-
 /**
  * comparator - compares each character of the string
  * @s: string
@@ -21,15 +20,14 @@ int _strlen_recursion(char *s)
  */
 int comparator(char *s, int n1, int n2)
 {
-	if (*(s + n1) == *(s + n2))
-	{
-		if (n1 == n2 || n1 == n2 + 1)
-			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
-	}
-	return (0);
+if (*(s + n1) == *(s + n2))
+{
+if (n1 == n2 || n1 == n2 + 1)
+return (1);
+return (0 + comparator(s, n1 + 1, n2 - 1));
 }
-
+return (0);
+}
 /**
  * is_palindrome - detects if a string is a palindrome
  * @s: string
@@ -37,8 +35,7 @@ int comparator(char *s, int n1, int n2)
  */
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+if (*s == '\0')
+return (1);
+return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
-
