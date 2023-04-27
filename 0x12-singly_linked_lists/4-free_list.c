d19eb877f52787706decb04a5cd02ccd016390e5
Mayaspath <mayaspath@gmail.com>
2023-04-27 13:01:23 -0700

    Write a function that frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: head of the linked list
+ * Return: no return
+ * Maya 27/04/23
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
