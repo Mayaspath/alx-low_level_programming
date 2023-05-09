63595003a811d37ee3ba81b1313ea17bcc69dbab
Mayaspath <mayaspath@gmail.com>
2023-05-09 05:26:51 -0700

    delete

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - reads atext file and prints the letters
+ * @filename: filename
+ * @letters: numbers of letters printed
+ * Maya 08/05/23
+ *
+ * Return: numbers of letters printed, If fail returns 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This directory is about C language basics
